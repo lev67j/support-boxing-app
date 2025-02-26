441b598ef18ebc9ded7aad7143a769c58db9adfc
Lev <153025879+lev67j@users.noreply.github.com>
2025-02-26 22:39:58 +0300

    good support

---
+++ Support Box App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Support Box App
+//
+//  Created by Lev Vlasov on 2025-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Support Box App/Support_Box_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Support_Box_AppApp.swift
+//  Support Box App
+//
+//  Created by Lev Vlasov on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Support_Box_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
